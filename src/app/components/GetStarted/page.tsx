d00f727d996c2ac3c39a086b22ba8b80878011bf
bedmuthaapoorv <apoorvbedmutha@gmail.com>
2024-03-18 18:16:46 +0530

    fixed UI bugs

--- src/app/components/GetStarted/page.tsx
+++ src/app/components/GetStarted/page.tsx
@@ -1,5 +1,4 @@
 import GetStarted from "./GetStarted"
-import Context from "@/app/services/context/context"
 export default function Page() {
     return (
         <GetStarted></GetStarted>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import config from "./resources/config";
 import Components from "./Components";
-import Context from "./services/context/context";
 export default async function Home() {
   return (
 
